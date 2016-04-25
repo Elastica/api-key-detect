9d6a25c8bbda007f052a8f19725714fcadb1a336
Chaks Chigurupati <chaks.chigurupati@elastica.co>
2016-04-25 11:45:37 -0700

    Made some changes that are relevant for Elastica: 1) Added more file types to the ignore list, 2) Print the surrounding text in front of the suspected key/token rather than the whole line, 3) look at a line only if it contains one of token, key, secret to minimize false positives

--- api_key_detect.py
+++ api_key_detect.py
@@ -1,8 +1,11 @@
 import sys, os, re, itertools
 
-ignored = ['.git', 'node_modules', 'bower_components', '.sass-cache', '.png', '.ico', '.mov']
+ignored = ['.git', 'node_modules', 'bower_components', '.sass-cache', '.png', '.ico', \
+            '.mov', '.pdf', '.bmp', '.wsdl', '.jar', '.json', '.exe', '.csv', '.jzb', \
+            '.gz', '.zip', '.tar', '.o', '.so', '.a', '.xml', '.dll', '.msi', '.key', '.crt' ]
+
 api_key_min_entropy_ratio = 0.5
-api_key_min_length = 7
+api_key_min_length = 15
 
 def pairwise(iterable):
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
@@ -27,11 +30,25 @@ def line_contains_api_key(line):
 	"""
 	Returns True if any token in the line contains an API key or password.
 	"""
-	for token in re.findall(r"[\w]+", line):
+	lline = line.lower()
+	if not (lline.find("token") != -1 or \
+	  lline.find("secret") != -1 or \
+	  lline.find("key") != -1):
+		return (False, '', '')
+
+	tokens = re.findall(r"[\w]+", line)
+	i = 0
+	for token in tokens:
 		result = token_is_api_key(token)
 		if result[0]:
-			return (True, result[1])
-	return (False, '')
+			ndx =  i - 3
+			if ndx < 0:
+				ndx = 0
+			snippet = ' '.join(tokens[ndx: i+1])
+			return (True, snippet, result[1])
+			#return (True, token, result[1])
+		i += 1
+	return (False, '', '')
 
 def scan_file(path_to_file):
 	"""
@@ -43,8 +60,9 @@ def scan_file(path_to_file):
 	for line in f:
 		result = line_contains_api_key(line)
 		if result[0]:
-			print '\033[1m' + path_to_file + ' : Line ' + str(number) + ' : Entropy ' + str(result[1]) + '\033[0m'
-			print line
+			print '\033[1m' + path_to_file + ' : Line ' + str(number) + ' : Entropy ' + str(result[2]) + '\033[0m'
+			print result[1]
+			#print line
 		number += 1
 
 def scan_dir(path):
@@ -63,7 +81,10 @@ def scan_dir(path):
 				if ignore in fullpath:
 					skip = True
 			if not skip:
-				scan_file(fullpath)
+				try:
+					scan_file(fullpath)
+				except:
+					pass
 
 if __name__ == "__main__":
 	if len(sys.argv) == 1:
